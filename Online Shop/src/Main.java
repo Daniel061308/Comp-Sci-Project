cc16bbc70f0a636bd9ad07cdd65c59f2aa1b8703
Lee Bloody Jack <440017013@gapps.yrdsb.ca>
2025-01-16 13:03:16 -0500

    Update Main.java

--- Online Shop/src/Main.java
+++ Online Shop/src/Main.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -7,6 +8,7 @@ public class Main {
         ArrayList<Integer> cartQuantity = new ArrayList<Integer>();
         ArrayList<Integer> cartCategory = new ArrayList<Integer>();
         ArrayList<Integer> cartItemNumber = new ArrayList<Integer>();
+        ArrayList<Integer> cartPrice = new ArrayList<Integer>();  // Changed to cartPrice
         ArrayList<String> History = new ArrayList<String>();
 
         ArrayList<String> sportProductName = new ArrayList<String>();
@@ -15,6 +17,9 @@ public class Main {
         ArrayList<Integer> sportProductAvailibity = new ArrayList<Integer>();
         sportProductAvailibity.add(1);
         sportProductAvailibity.add(3);
+        ArrayList<Integer> sportProductPrice = new ArrayList<Integer>();
+        sportProductPrice.add(10);
+        sportProductPrice.add(30);
 
         ArrayList<String> clothingProductName = new ArrayList<String>();
         clothingProductName.add("hat");
@@ -22,6 +27,9 @@ public class Main {
         ArrayList<Integer> clothingProductAvailibity = new ArrayList<Integer>();
         clothingProductAvailibity.add(1);
         clothingProductAvailibity.add(10);
+        ArrayList<Integer> clothingProductPrice = new ArrayList<Integer>(); // Added price for clothing
+        clothingProductPrice.add(5);  // Example price for hat
+        clothingProductPrice.add(20); // Example price for shoe
 
         ArrayList<String> bookProductName = new ArrayList<String>();
         bookProductName.add("book1");
@@ -29,9 +37,11 @@ public class Main {
         ArrayList<Integer> bookProductAvailibity = new ArrayList<Integer>();
         bookProductAvailibity.add(1);
         bookProductAvailibity.add(2);
+        ArrayList<Integer> bookProductPrice = new ArrayList<Integer>(); // Added price for books
+        bookProductPrice.add(15);  // Example price for book1
+        bookProductPrice.add(25);  // Example price for book3
 
-
-        final String ADMIN_PASS = "Hello World";
+        String ADMIN_PASS = "Hello World";
         boolean running = true;
         while (running) {
             System.out.println("Welcome to the Shop!");
@@ -41,6 +51,7 @@ public class Main {
             System.out.print("Choose an option: ");
             int MainChoice = sc.nextInt();
             if (MainChoice == 1) {
+                sc.nextLine(); // Consume newline left-over
                 System.out.print("Enter admin password: ");
                 String password = sc.nextLine();
                 if (!password.equals(ADMIN_PASS)) {
@@ -66,104 +77,155 @@ public class Main {
                     System.out.println("4. Shopping Cart");
                     System.out.println("5. Exit to Main Menu");
                     System.out.print("Choose a category: ");
-                    
-                    //start from here, and make the functionality for each shop category and the shopping cart within the while (shopping)
-                    //loop, including item selection, adding to the cart, and navigating back to the main menu.
+
                     int shopChoice = sc.nextInt();
-                    if(shopChoice == 1){
-                         System.out.println("items:");
-                        for(int ii = 0; sportProductAvailibity.size()>ii;ii++){
-                           //remember to use printf
-                            System.out.printf("%d %s Availability %d %n",ii+1, sportProductName.get(ii), sportProductAvailibity.get(ii));
+                    if (shopChoice == 1) {
+                        System.out.println("Items:");
+                        for (int ii = 0; ii < sportProductAvailibity.size(); ii++) {
+                            int availability = sportProductAvailibity.get(ii);
+                            String productName = sportProductName.get(ii);
+                            if (availability > 0) {
+                                System.out.printf("%d. %s - Availability: %d - Price: $%d%n", ii + 1, productName, availability, sportProductPrice.get(ii));
+                            } else {
+                                System.out.printf("%d. %s - Out of stock%n", ii + 1, productName);
+                            }
                         }
-                        System.out.println("select items");
+                        System.out.println("Select an item:");
                         int AddCartItem = sc.nextInt();
-                        if(AddCartItem > 0 && AddCartItem <= sportProductName.size()){
-                            while(true){
-                                System.out.println("List the amount you would like to purchase");
-                                int Wants = sc.nextInt();
-                                if(Wants > sportProductAvailibity.get(AddCartItem-1)){
-                                    System.out.println("Not enough items to fulfill the order");
-                                    }else{
-                                        cartName.add(sportProductName.get(AddCartItem-1));
+                        if (AddCartItem > 0 && AddCartItem <= sportProductName.size()) {
+                            int itemIndex = AddCartItem - 1;
+                            if (sportProductAvailibity.get(itemIndex) > 0) {
+                                while (true) {
+                                    System.out.println("List the amount you would like to purchase:");
+                                    int Wants = sc.nextInt();
+                                    if (Wants > sportProductAvailibity.get(itemIndex)) {
+                                        System.out.println("Not enough items to fulfill the order");
+                                    } else {
+                                        cartName.add(sportProductName.get(itemIndex));
                                         cartQuantity.add(Wants);
-                                        cartcategory.add(0);
-                                        cartItemNumber.add(AddCartNumber-1);
-                                        sportProductAvailibity.set(AddCartItem-1, sportProductAvailibity.get(AddCartItem-1)-Wants);
+                                        cartCategory.add(0);
+                                        cartItemNumber.add(itemIndex);
+                                        int itemPrice = sportProductPrice.get(itemIndex) * Wants;
+                                        cartPrice.add(itemPrice); // Calculate and add price to cart
+                                        sportProductAvailibity.set(itemIndex, sportProductAvailibity.get(itemIndex) - Wants);
                                         System.out.println("Item has been added to your cart");
                                         break;
                                     }
+                                }
+                            } else {
+                                System.out.println("This item is out of stock.");
                             }
                         }
-                    }
-                    else if(shopChoice==2){
-                        
-                    }else if(shopChoice==3){
-                        
-                    }else if(shopChoice==4){
-                        System.out.println("shopping cart items:");
-                        for(int ii = 0; cartName.size()>ii;ii++){
-                            System.out.printf("%d %s Item: %d %n",ii+1,cartName.get(ii),cartQuantity.get(ii));
+                    } else if (shopChoice == 2) {
+                        System.out.println("Items:");
+                        for (int ii = 0; ii < clothingProductAvailibity.size(); ii++) {
+                            int availability = clothingProductAvailibity.get(ii);
+                            String productName = clothingProductName.get(ii);
+                            if (availability > 0) {
+                                System.out.printf("%d. %s - Availability: %d - Price: $%d%n", ii + 1, productName, availability, clothingProductPrice.get(ii));
+                            } else {
+                                System.out.printf("%d. %s - Out of stock%n", ii + 1, productName);
+                            }
                         }
+                        System.out.println("Select an item:");
+                        int AddCartItem = sc.nextInt();
+                        if (AddCartItem > 0 && AddCartItem <= clothingProductName.size()) {
+                            int itemIndex = AddCartItem - 1;
+                            if (clothingProductAvailibity.get(itemIndex) > 0) {
+                                while (true) {
+                                    System.out.println("List the amount you would like to purchase:");
+                                    int Wants = sc.nextInt();
+                                    if (Wants > clothingProductAvailibity.get(itemIndex)) {
+                                        System.out.println("Not enough items to fulfill the order");
+                                    } else {
+                                        cartName.add(clothingProductName.get(itemIndex));
+                                        cartQuantity.add(Wants);
+                                        cartCategory.add(1);
+                                        cartItemNumber.add(itemIndex);
+                                        int itemPrice = clothingProductPrice.get(itemIndex) * Wants;
+                                        cartPrice.add(itemPrice); // Calculate and add price to cart
+                                        clothingProductAvailibity.set(itemIndex, clothingProductAvailibity.get(itemIndex) - Wants);
+                                        System.out.println("Item has been added to your cart");
+                                        break;
+                                    }
+                                }
+                            } else {
+                                System.out.println("This item is out of stock.");
+                            }
+                        }
+                    } else if (shopChoice == 3) {
+                        System.out.println("Items:");
+                        for (int ii = 0; ii < bookProductAvailibity.size(); ii++) {
+                            int availability = bookProductAvailibity.get(ii);
+                            String productName = bookProductName.get(ii);
+                            if (availability > 0) {
+                                System.out.printf("%d. %s - Availability: %d - Price: $%d%n", ii + 1, productName, availability, bookProductPrice.get(ii));
+                            } else {
+                                System.out.printf("%d. %s - Out of stock %n", ii + 1, productName);
+                            }
+                        }
+                        System.out.println("Select an item:");
+                        int AddCartItem = sc.nextInt();
+                        if (AddCartItem > 0 && AddCartItem <= bookProductName.size()) {
+                            int itemIndex = AddCartItem - 1;
+                            if (bookProductAvailibity.get(itemIndex) > 0) {
+                                while (true) {
+                                    System.out.println("List the amount you would like to purchase:");
+                                    int Wants = sc.nextInt();
+                                    if (Wants > bookProductAvailibity.get(itemIndex)) {
+                                        System.out.println("Not enough items to fulfill the order");
+                                    } else {
+                                        cartName.add(bookProductName.get(itemIndex));
+                                        cartQuantity.add(Wants);
+                                        cartCategory.add(2);
+                                        cartItemNumber.add(itemIndex);
+                                        int itemPrice = bookProductPrice.get(itemIndex) * Wants;
+                                        cartPrice.add(itemPrice); // Calculate and add price to cart
+                                        bookProductAvailibity.set(itemIndex, bookProductAvailibity.get(itemIndex) - Wants);
+                                        System.out.println("Item has been added to your cart");
+                                        break;
+                                    }
+                                }
+                            } else {
+                                System.out.println("This item is out of stock.");
+                            }
+                        }
+                    } else if (shopChoice == 4) {
+                        System.out.println("Shopping Cart Items:");
+                        int totalPrice = 0;
+                        for (int ii = 0; ii < cartName.size(); ii++) {
+                            System.out.printf("%d. %s - Quantity: %d - Price: $%d%n", ii + 1, cartName.get(ii), cartQuantity.get(ii), cartPrice.get(ii));
+                            totalPrice += cartPrice.get(ii);
+                        }
+                        System.out.printf("Total Price: $%d%n", totalPrice);
                         System.out.println("Pick an option:");
                         System.out.println("1. Check out");
                         System.out.println("2. Update Cart");
                         System.out.println("3. Exit to shopping menu");
                         int CheckChoice = sc.nextInt();
-                        if(CheckChoice==1){
-                            
-                        }else if(CheckChoice==2){
-                            System.out.println("what do you want to update?");
-                            int AddCartItem = sc.nextInt();
-                        if(AddCartItem > 0 && AddCartItem <= CartName.size()){
-                            
-                            while(true){
-                                System.out.println("List the amount you would like to purchase");
-                                int Wants = sc.nextInt();
-                                if(Wants < 0){
-                                    System.out.println("Invaild Input");
-                                    
-                                }else if(Wants==0){
-                                    System.out.println("Item has been removed");
-                                    CartName.remove(AddCartItem-1);
-                                    CartQuanity.remove(AddCartItem-1);
-                                    break;
-                                }else if (Wants>0){
-                                    if(Wants>CartQuanity.get(AddCartItem-1)){
-                                        System.out.println("Not enough items to remove");
-                                    }else if(Wants == CartQuanity.get(AddCartItem-1)){
-                                        System.out.println("nothing happened");
-                                    }else if(Wants< CartQuanity.get(AddCartItem-1)){
-                                        System.out.println("the item has changed to your wanted amount");
-                                        CartQuanity.set(AddCartItem,Wants);
-                                        if(cartCategory.get(AddCartItem-1) == 0){
-                                            sportProductAvailability.set(cartItemNumber.get(AddCartItem-1),CartQuanity.get(AddCartItem-1)-Wants);
-                                        }
-                                    }
-                                    }
-                                    CartQuanity.set(,CartQuanity.get(AddCartItem)-Wants);
-                                }
-                                if(Wants > CartQuanity.get(AddCartItem-1)){
-                                    System.out.println("Not enough items to fulfill the order");
-                                    }else{
-                                        cartName.add(sportProductName.get(AddCartItem-1));
-                                        cartQuantity.add(Wants);
-                                        sportProductAvailibity.set(AddCartItem-1, sportProductAvailibity.get(AddCartItem-1)-Wants);
-                                        System.out.println("Item has been added to your cart");
-                                        break;
-                                    }
-                            }
-                        }else if (CheckChoice==3){
-                            
+                        if (CheckChoice == 1) {
+                            System.out.println("You are about to checkout with a total of $" + totalPrice);
+                            // Here you could add the logic for completing the purchase and adding to history
+                            History.add("Purchase: $" + totalPrice);
+                            cartName.clear();
+                            cartQuantity.clear();
+                            cartCategory.clear();
+                            cartItemNumber.clear();
+                            cartPrice.clear();
+                            System.out.println("Thank you for your purchase!");
+                        } else if (CheckChoice == 2) {
+                            // Update cart
+                        } else if (CheckChoice == 3) {
                         }
+                    } else if (shopChoice == 5) {
+                        shopping = false;
                     }
                 }
             } else if (MainChoice == 3) {
                 System.out.println("Thanks for shopping.");
                 running = false;
-            } else {
-                System.out.println("Invalid input.");
             }
         }
+        sc.close();
     }
 }
